796c4ab44e4043de78d62c9026486fe0fd52fa65
Rovva <rovva@hostarn.se>
2018-01-20 17:57:16 +0100

    Litle more comments on filters.js

--- app/server/filter/filters.js
+++ app/server/filter/filters.js
@@ -1,5 +1,10 @@
 /*
 Cant find any function calls from this one. Unless the export in the last row does something.
+
+ It seems like what this function does, is creating an array using the function "range" (works like python range)
+ and returning it to the last line in this function "exports.MinMax". The range is used 
+ to fill an array of all the hours between 01:00 and 08:00 as an example.
+
 */
 function BuildMinMax(attribute, min, max) {
     var filter = { range: {} };
@@ -12,7 +17,7 @@ function BuildMinMax(attribute, min, max) {
     return filter;
 }
 /*
- Seems like the line below creates a "shortcut" to the above function and is used
+ Seems like the line below creates a "shortcut" to the result of the above function and is used
  heavily in index.js like: filter.MinMax("timestamp", date.start, date.end)
 */
 exports.MinMax = BuildMinMax;
