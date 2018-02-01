e0bae97ed2c2814ce0aac5e60145f90ba55e980c
grupp9 <grupp9@B1220.lab.ltu.se>
2018-02-01 12:01:06 +0100

    lineargraph implemented with hardcoded data

--- app/client/app/Charts.js
+++ app/client/app/Charts.js
@@ -1,3 +1,5 @@
+//var csv = require("./data.csv")
+
 
 /*
 It should return a color depending of case, used by the charts functions in this file.
@@ -50,12 +52,10 @@ function drawChart(type,data) {
             drawHistogram(data);
             break;
         }
-/*
         case "linearGraph": {
             drawLinearGraph(data);
             break;
         }
-*/
     }
 }
 
@@ -64,7 +64,75 @@ Uses D3JS to create a chart instead of Plotly.js.
 */
 
 function drawLinearGraph(data){
-    
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// parse the date / time
+var parseTime = d3.timeParse("%d-%b-%y");
+
+// set the ranges
+var x = d3.scaleTime().range([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
+
+
+
+// define the line
+var valueline = d3.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.close); });
+
+// append the svg obgect to the body of the page
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("#graphs_container").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+// Get the data
+d3.csv("app/data.csv", function(error, data) {
+  if (error) throw error;
+
+console.log(data[0]);
+
+  // format the data
+  data.forEach(function(d) {
+      d.date = parseTime(d.date);
+      d.close = +d.close;
+  });
+
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+  // Add the valueline path.
+  svg.append("path")
+      .data([data])
+      .attr("class", "line")
+      .attr("d", valueline);
+
+  // Add the X Axis
+  svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  // Add the Y Axis
+  svg.append("g")
+      .call(d3.axisLeft(y));
+
+});    
+
+ var btn = document.getElementById("graphsButton");
+    $(btn).toggleClass("graphs-button-open",true);
+    $("#graphs_div").toggleClass("graphs-div-open",true);
+    //document.getElementById('graphs_container').appendChild(svg);
+    window.scrollTo(0, document.getElementById("graphs_container").getBoundingClientRect().bottom);
 }
 
 
