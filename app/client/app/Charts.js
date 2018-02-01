04548f8590ab9524ad68851b6fc24089859615e6
grupp9 <grupp9@B1220.lab.ltu.se>
2018-02-01 12:13:07 +0100

    fixed size of lineargraph

--- app/client/app/Charts.js
+++ app/client/app/Charts.js
@@ -65,9 +65,9 @@ Uses D3JS to create a chart instead of Plotly.js.
 
 function drawLinearGraph(data){
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width = 600 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
 
 // parse the date / time
 var parseTime = d3.timeParse("%d-%b-%y");
