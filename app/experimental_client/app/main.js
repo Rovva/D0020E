87aa98cd3bab339ee4e35cdf8568830e94764e81
Christoffer Rova <rovva@hostarn.se>
2018-02-22 20:06:58 +0100

    Edited some small things

--- app/experimental_client/app/main.js
+++ app/experimental_client/app/main.js
@@ -464,6 +464,7 @@ function saveFilter(filterObj) {
 
 function toggleMenu (btn) {
     //$('submit1').height($('submit1').width());
+    // Line below makes the site crash, has something to do with the 3 graphbuttons
     //var submitButton = document.getElementById('submit1');
     //console.log(JSON.stringify($('submit1').height()));
     //submitButton.style.width = submitButton.style.height.toString();
