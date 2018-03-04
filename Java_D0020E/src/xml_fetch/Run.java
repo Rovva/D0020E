21f595e7f8e8d4463d807ede43de153421201afa
Rovva <rovva@hostarn.se>
2018-03-04 18:03:12 +0100

    Sleep problems

--- Java_D0020E/src/xml_fetch/Run.java
+++ Java_D0020E/src/xml_fetch/Run.java
@@ -27,7 +27,7 @@ public class Run {
 			format.combineData();
 			mysql.readData();
 			mysql.insertToMysql();
-			Thread.sleep(6 *
+			sleep(6 *
 					 60 *   // minutes to sleep
 		             60 *   // seconds to a minute
 		             1000); // milliseconds to a second
